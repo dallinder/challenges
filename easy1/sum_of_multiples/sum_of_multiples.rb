e35857f702f5e09fb05d00de7eaf3d90dce714f3
dallinder <dcallinder1s@gmail.com>
2017-10-05 18:28:49 -0600

    solved the sum of multiples problem and passed all tests

--- easy1/sum_of_multiples/sum_of_multiples.rb
+++ easy1/sum_of_multiples/sum_of_multiples.rb
@@ -1,20 +1,44 @@
-# from 0 to a number 
-# find mutiples of 3 and 5
-# find the sum of those multiples
-# default arg set to 3 and 5
+# find sum of mulitples from 1 to num - 1
+# default args are 3 and 5
+  # find multiples of 3 OR 5
+# can specify as many arguments as you want
+
+# Approach
+# create a range for 1 to the input number
+# create a class method called 'to' with arg of number to go to
+# when initialze, *args for as many opt args
+# create .to instance method for when there are multi args
 
 class SumOfMultiples
-
-  def initialize(arg1=3, arg2=5, *args)
-    @arg1 = arg1
-    @arg2 = arg2
-    
+  def initialize(*args)
+    @args = args
   end
 
-  def self.to(num)
-    range = (1...num)
-    range.select { |num| num % @arg1 == 0 || num % @arg2 == 0 }.inject(:+)
+  def self.to(limit, mult = [3, 5])
+    range = (0...limit).to_a
+    results = []
+
+    range.each do |number|
+      if number % mult[0] == 0 || number % mult[1] == 0
+        results << number
+      end
+    end
+
+    results.inject(:+)
+  end
+
+  def to(limit)
+    range = (0...limit).to_a
+    results = []
+
+    @args.each do |multi|
+      range.each do |number|
+        if number % multi == 0 
+          results << number
+        end
+      end
+    end
+    results.uniq.inject(:+)
   end
 end
 
-SumOfMultiples.to(10)
--- easy1/sum_of_multiples/sum_of_multiples_test.rb
+++ easy1/sum_of_multiples/sum_of_multiples_test.rb
@@ -1,5 +1,8 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
 require_relative 'sum_of_multiples'
+
+MiniTest::Reporters.use!
 
 class SumTest < Minitest::Test
   def test_sum_to_1
@@ -27,22 +30,22 @@ class SumTest < Minitest::Test
   end
 
   def test_configurable_7_13_17_to_20
-    skip
+    # skip
     assert_equal 51, SumOfMultiples.new(7, 13, 17).to(20)
   end
 
   def test_configurable_4_6_to_15
-    skip
+    # skip
     assert_equal 30, SumOfMultiples.new(4, 6).to(15)
   end
 
   def test_configurable_5_6_8_to_150
-    skip
+    # skip
     assert_equal 4419, SumOfMultiples.new(5, 6, 8).to(150)
   end
 
   def test_configurable_43_47_to_10000
-    skip
+    # skip
     assert_equal 2_203_160, SumOfMultiples.new(43, 47).to(10_000)
   end
 end
