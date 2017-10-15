44ae050a649561392164eccc599970f8379132f6
dallinder <dcallinder1s@gmail.com>
2017-10-15 17:32:26 -0600

    made small changes to the way the hash appears, fixed comment where roman numeral 10 was missing

--- easy1/roman_numerals/roman_numerals.rb
+++ easy1/roman_numerals/roman_numerals.rb
@@ -1,5 +1,6 @@
 # I = 1
-# X = 5
+# V = 5
+# X = 10
 # L = 50
 # C = 100
 # D = 500
@@ -11,8 +12,14 @@
 
 # take a integer, return a roman numeral string
 
+# original attempt
+
 class Integer
-  ROMANS = { 1=>'I', 2=>'II', 3=>'III', 4=>'IV', 5=>'V', 6=>'VI', 7=>'VII', 8=>'VIII', 9=>'IX', 10=>'X', 20=>'XX', 30=>'XXX', 40=>'XL', 50=>'L', 60=>'LX', 70=>'LXX', 80=>'LXX', 90=>'XC', 100=>'C', 200=>'CC', 300=>'CCC', 400=>'CD', 500=>'D', 600=>'DC', 700=>'DCC', 800=>'DCCC', 900=>'CM', 1000=>'M', 2000=>'MM', 3000=>'MMM' }
+  ROMANS = { 1=>'I', 2=>'II', 3=>'III', 4=>'IV', 5=>'V', 6=>'VI', 7=>'VII',
+             8=>'VIII', 9=>'IX', 10=>'X', 20=>'XX', 30=>'XXX', 40=>'XL',
+             50=>'L', 60=>'LX', 70=>'LXX', 80=>'LXX', 90=>'XC', 100=>'C',
+             200=>'CC', 300=>'CCC', 400=>'CD', 500=>'D', 600=>'DC', 700=>'DCC',
+             800=>'DCCC', 900=>'CM', 1000=>'M', 2000=>'MM', 3000=>'MMM' }
   def to_roman
     result = []
     number = self.to_s.chars.reverse
@@ -24,3 +31,7 @@ class Integer
     end.join
   end
 end
+
+
+
+p 93.to_roman
